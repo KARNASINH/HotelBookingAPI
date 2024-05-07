97967c486356e25ae9d8aba40e56343aae754a4e
Karnasinh <karnasinh281@gmail.com>
2024-05-07 15:01:28 -0400

    Adds SqlConnectionFactory class that will be used to get and establish connection request.

---
+++ HotelBookingAPI/Connection/SqlConnectionFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace HotelBookingAPI.Connection
+{
+    //This class get a connection string from appsettings.json file to establish the connection with Database.
+    public class SqlConnectionFactory
+    {
+        //Creating IConfiguration object to get value based on the key from appsettings.json file.
+        private readonly IConfiguration _configuration;
+
+        //Injecting IConfiguration objerct using DI.
+        public SqlConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //
+        public SqlConnection CreateConnection() => new SqlConnection(_configuration.GetConnectionString("DefaultConnection");           
+       
+    }
+}
