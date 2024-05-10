8bce455d29ece7eb58a4e0a62c554d485148f914
Karnasinh <karnasinh281@gmail.com>
2024-05-09 22:55:39 -0400

    Adds class to which takes the information to create a user into Database.

---
+++ HotelBookingAPI/DTOs/UserDTOs/CreateUserDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.UserDTOs
+{
+    //This class is used to create a new user.
+    //It contains the email and password of the user.
+    public class CreateUserDTO
+    {
+        //Email of the user
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        //Password of the user
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; } //Before sending password to the server, it should be hashed.
+    }
+}
