81f10817cf7a9e07451ca35bdb99f271f02018a4
Karnasinh <karnasinh281@gmail.com>
2024-05-09 23:28:39 -0400

    Adds UpdateUserDTO class to update the User infromation into the Database.

---
+++ HotelBookingAPI/DTOs/UserDTOs/UpdateUserDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.UserDTOs
+{
+    //This class is used to update a user.
+    public class UpdateUserDTO
+    {
+        //UserID of the user
+        [Required(ErrorMessage ="UserID is required")]
+        public int UserID { get; set; }
+
+        //Email of the user
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        //Password of the user
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
