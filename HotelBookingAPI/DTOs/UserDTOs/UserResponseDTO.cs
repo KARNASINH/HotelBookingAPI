e913191acca1f801476fc1ff4ba8656dd06cb81c
Karnasinh <karnasinh281@gmail.com>
2024-05-10 09:14:16 -0400

    Adds UserResponseDTO class that is used to return the user data to the client.

---
+++ HotelBookingAPI/DTOs/UserDTOs/UserResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelBookingAPI.DTOs.UserDTOs
+{
+    //UserResponseDTO class is used to return the user data to the client
+    public class UserResponseDTO
+    {
+        //User Id
+        public string UserId { get; set; }
+
+        //User Email Address
+        public string Email { get; set; }
+
+        //User is active or not
+        public bool IsActive { get; set; }
+
+    }
+}
