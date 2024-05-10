d256118b96007f59b18ea9c0f6bd8050f506d959
Karnasinh <karnasinh281@gmail.com>
2024-05-09 23:43:36 -0400

    Adds UserRoleDTO class to assign a role to the given user.

---
+++ HotelBookingAPI/DTOs/UserDTOs/userRoleDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.UserDTOs
+{
+    //This class holds the information to update the user role
+    public class UserRoleDTO
+    {
+        //UserID of the user
+        [Required(ErrorMessage = "UserID is required")]       
+        public int UserID { get; set; }
+
+        //RoleID of the user
+        [Required(ErrorMessage = "RoleID is required")]
+        public int RoleID { get; set; }
+    }
+}
