25c2d8104f0e0ae41df8a8ef800a21eb27a0d942
Karnasinh <karnasinh281@gmail.com>
2024-05-09 23:40:01 -0400

    Adds a LogInUserDTO class which hold the information of the User required for the LogIn.

---
+++ HotelBookingAPI/DTOs/UserDTOs/LogInUserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.UserDTOs
+{
+    //This class holds the information about the user that is trying to log in
+    public class LogInUserDTO
+    {
+        //Email of the user
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        //Password of the user
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
