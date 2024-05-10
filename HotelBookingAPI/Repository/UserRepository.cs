3d676300684aecd1fdfc75774928456a71534563
Karnasinh <karnasinh281@gmail.com>
2024-05-10 09:21:35 -0400

    Creates an instance of SqlConnectionFactory to esablish the connection to the database.

--- HotelBookingAPI/Repository/UserRepository.cs
+++ HotelBookingAPI/Repository/UserRepository.cs
@@ -5,7 +5,7 @@ namespace HotelBookingAPI.Repository
     //UserRepository class is used to interact with the database to perform CRUD operations on the User table
     public class UserRepository
     {
-        //SqlConnectionFactory instance
+        //SqlConnectionFactory instance to create a connection to the database
         private readonly SqlConnectionFactory _connectionFactory;
 
         //This constructor is used to inject the SqlConnectionFactory instance
