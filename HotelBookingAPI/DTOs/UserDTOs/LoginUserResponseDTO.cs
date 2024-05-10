b5e9e15cafdd0d9772789c625c33b9b791bf1af1
Karnasinh <karnasinh281@gmail.com>
2024-05-10 00:04:05 -0400

    Adds LoginUserResponseDTO class tohold the generated response when a user logs in.

---
+++ HotelBookingAPI/DTOs/UserDTOs/LoginUserResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.UserDTOs
+{
+    //This class is used to generate a response when a user logs in.
+    public class LoginUserResponseDTO
+    {
+        //Id of the user
+        public int UserId { get; set; }
+
+        //Email of the user
+        public string Message { get; set; }
+
+        //Is the user logged in
+        public bool IsLogin { get; set; }
+    }
+}
