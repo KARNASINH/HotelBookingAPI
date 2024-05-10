7f9dc5bb83869ff833921270a91cb44bd3223b78
Karnasinh <karnasinh281@gmail.com>
2024-05-10 09:04:24 -0400

    Adds DeleteUserResponseDTO class which holds the infromation after performing the delete operation on the database.

---
+++ HotelBookingAPI/DTOs/UserDTOs/DeleteUserResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelBookingAPI.DTOs.UserDTOs
+{
+    // DeleteUserResponseDTO class is used to return the response when the User is deleted from the database.
+    public class DeleteUserResponseDTO
+    {
+        //UserId to hold the id of the user that is deleted.
+        public int UserId { get; set; }
+
+        // Message to hold the response while perdorming the delete operation.
+        public string Message { get; set; }
+    }
+}
