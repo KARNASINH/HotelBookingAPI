6266de7ff9db7983e846e89dd0b850f938d4aeca
Karnasinh <karnasinh281@gmail.com>
2024-05-10 09:19:40 -0400

    Adds UserRepository class which used to interact with the database to perform CRUD operations on the User table.

---
+++ HotelBookingAPI/Repository/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using HotelBookingAPI.Connection;
+
+namespace HotelBookingAPI.Repository
+{
+    //UserRepository class is used to interact with the database to perform CRUD operations on the User table
+    public class UserRepository
+    {
+        //SqlConnectionFactory instance
+        private readonly SqlConnectionFactory _connectionFactory;
+
+        //This constructor is used to inject the SqlConnectionFactory instance
+        public UserRepository(SqlConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+    }
+}
