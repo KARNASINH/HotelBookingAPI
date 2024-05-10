bf1204d7c7c81785d62130555375302013d08f9f
Karnasinh <karnasinh281@gmail.com>
2024-05-10 00:21:25 -0400

    Adda UserRoleResponseDTO class to generates response when a role is assigned to the user.

---
+++ HotelBookingAPI/DTOs/UserDTOs/UserRoleResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelBookingAPI.DTOs.UserDTOs
+{
+    //This class is used to generate a response when a user role is assigned.
+    public class UserRoleResponseDTO
+    {
+        //Message to be displayed if any error occurs
+        public string Message { get; set; }
+
+        //Is the user role assigned
+        public bool IsRoleAssigned { get; set; }
+    }
+}
