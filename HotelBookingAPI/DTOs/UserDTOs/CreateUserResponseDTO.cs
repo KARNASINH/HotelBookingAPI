818429a51acae73b01eb60ff8a4a4ab5790ccc4b
Karnasinh <karnasinh281@gmail.com>
2024-05-09 23:56:28 -0400

    Adds CreateUserResponseDTO class to hold the information after creating a new user.

---
+++ HotelBookingAPI/DTOs/UserDTOs/CreateUserResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.UserDTOs
+{
+    //This class generates a response after creating a new user.
+    public class CreateUserResponseDTO
+    {
+        //UserID of the user
+        public int UserId { get; set; }
+
+        //Message to be displayed if any error occurs
+        public string Message { get; set; }
+
+        //Flag to check if the user is created
+        public bool IsCreated { get; set; }
+    }
+}
