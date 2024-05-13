5a10015f89247792f2d55023b70a2bbebccfda51
Karnasinh <karnasinh281@gmail.com>
2024-05-13 17:28:00 -0400

    Adds UpdateRoomTypeResponseDTO class to hold the response data while updating RoomType information into database.

---
+++ HotelBookingAPI/DTOs/RoomTypeDTOs/UpdateRoomTypeResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace HotelBookingAPI.DTOs.RoomTypeDTOs
+{
+    //This class is used to hold the response data while updating RoomType information
+    public class UpdateRoomTypeResponseDTO
+    {
+        //Hold the id of the updated RoomType
+        public int RoomTypeId { get; set; }
+
+        //Hold the Successful or Unsuccessful message while updating RoomType information
+        public string Message { get; set; }
+
+        //Hold the StatusCode while updating RoomType information
+        public bool IsUpdated { get; set; }
+    }
+}
