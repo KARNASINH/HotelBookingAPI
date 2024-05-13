cfe531f10e5369b96860ea70667b3a364b62c996
Karnasinh <karnasinh281@gmail.com>
2024-05-13 17:06:20 -0400

    Adds CreateRoomTypeResponseDTO class which holds response information for the RoomType creation.

---
+++ HotelBookingAPI/DTOs/RoomTypeDTOs/CreateRoomTypeResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace HotelBookingAPI.DTOs.RoomTypeDTOs
+{
+    //This class is used to hold the response data while creating a new RoomType
+    public class CreateRoomTypeResponseDTO
+    {
+        //Hold the id of the newly created RoomType
+        public int RoomTypeId { get; set; }
+
+        //Hold the Successful or Unsuccessful message while creating a new RoomType
+        public string Message { get; set; }
+
+        //Hold the StatusCode while creating a new RoomType
+        public bool IsCreated { get; set; }
+    }
+}
