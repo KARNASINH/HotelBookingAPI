0afeb6078a33a47443a6014d74f932b8e5959a07
Karnasinh <karnasinh281@gmail.com>
2024-05-13 16:44:22 -0400

    Adds CreateRoomTypeDTO class to hold the infromation to Create the RoomType.

---
+++ HotelBookingAPI/DTOs/RoomTypeDTOs/CreateRoomTypeDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.RoomTypeDTOs
+{
+    // Data Transfer Object for creating a new RoomType
+    public class CreateRoomTypeDTO
+    {
+        //Hold the name of the room type
+        [Required]
+        public string TypeName { get; set; }
+
+        //Hold the Accessibility features of the room type
+        [Required]
+        public string AccessibilityFeatures { get; set; }
+
+        //Hold the description of the room type
+        [Required]
+        public string Description { get; set; }
+
+        //Hold the Active status of the room type
+        [Required]
+        public bool IsActive { get; set; }
+    }
+}
