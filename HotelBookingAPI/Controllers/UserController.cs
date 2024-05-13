419ce3126024d211049bd2e9b9d90995c617b9ba
Karnasinh <karnasinh281@gmail.com>
2024-05-13 10:16:16 -0400

    Adds UserController which holds all the endpoints related to User operations.

---
+++ HotelBookingAPI/Controllers/UserController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookingAPI.Controllers
+{
+    //API Controller for User which holds all the endpoints related to User operations.
+    public class UserController : Controller
+    {
+        
+    }
+}
