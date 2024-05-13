cef92e7781fcc8058f9416c569465a5f41970fdb
Karnasinh <karnasinh281@gmail.com>
2024-05-13 16:23:23 -0400

    Updated comments for RoomTypeDTO class.

--- HotelBookingAPI/DTOs/RoomTypeDTOs/RoomTypeDTO.cs
+++ HotelBookingAPI/DTOs/RoomTypeDTOs/RoomTypeDTO.cs
@@ -1,11 +1,21 @@
 ﻿namespace HotelBookingAPI.DTOs.RoomTypeDTOs
 {
+    // This class is used to represent RoomType's information.
     public class RoomTypeDTO
     {
+        //The RoomType's ID.
         public int RoomTypeID { get; set; }
+
+        //The RoomType's name.
         public string TypeName { get; set; }
+        
+        //Accessibility features available to this RoomType.
         public string AccessibilityFeatures { get; set; }
+
+        //The RoomType's description.
         public string Description { get; set; }
+
+        //The RoomType is acrive or not.
         public bool IsActive { get; set; }
     }
 }
