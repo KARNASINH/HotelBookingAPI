84fabe389b29c707638fe221203576e474d75c09
Karnasinh <karnasinh281@gmail.com>
2024-05-13 11:19:54 -0400

    Adds End point to add the user into the database.

--- HotelBookingAPI/Controllers/UserController.cs
+++ HotelBookingAPI/Controllers/UserController.cs
@@ -1,5 +1,8 @@
-﻿using HotelBookingAPI.Repository;
+﻿using HotelBookingAPI.DTOs.UserDTOs;
+using HotelBookingAPI.Models;
+using HotelBookingAPI.Repository;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace HotelBookingAPI.Controllers
 {
@@ -19,6 +22,50 @@ namespace HotelBookingAPI.Controllers
         {
             _userRepository = userRepository;
             _logger = logger;
+        }
+
+        //API Endpoint to add a new user.
+        [HttpPost("AddUser")]
+        public async Task<APIResponse<CreateUserResponseDTO>> AddUser(CreateUserDTO createUserDTO)
+        {
+            //Log the request received for AddUser.
+            _logger.LogInformation("Request Received for AddUser: {@CreateUserDTO}", createUserDTO);
+
+            //Check if the request body is valid.
+            if (!ModelState.IsValid)
+            {
+                _logger.LogInformation("Invalid Data in the Requrest Body");
+
+                //Return Bad Request if the request body is invalid.
+                return new APIResponse<CreateUserResponseDTO>(HttpStatusCode.BadRequest, "Invalid Data in the Requrest Body");
+            }
+
+            //Try to add the user to the database.
+            try
+            {
+                //Call the AddUserAsync method from UserRepository to add the user.
+                var response = await _userRepository.AddUserAsync(createUserDTO);
+
+                _logger.LogInformation("AddUser Response From Repository: {@CreateUserResponseDTO}", response);
+
+                //Check if the user is created successfully.
+                if (response.IsCreated)
+                {
+                    //Return the response with the user details.
+                    return new APIResponse<CreateUserResponseDTO>(response, response.Message);
+                }
+
+                //Return the response with the error message.
+                return new APIResponse<CreateUserResponseDTO>(HttpStatusCode.BadRequest, response.Message);
+            }
+            //Catch the exception if any error occurs.
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error adding new user with email {Email}", createUserDTO.Email);
+
+                //Return Internal Server Error if any error occurs during the execion of the Action Method.
+                return new APIResponse<CreateUserResponseDTO>(HttpStatusCode.InternalServerError, "Registration Failed.", ex.Message);
+            }
         }
     }
 }
