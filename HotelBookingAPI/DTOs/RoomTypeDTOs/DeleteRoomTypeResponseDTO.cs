6362c253ffc37126840c2a752a2d46e27b845733
Karnasinh <karnasinh281@gmail.com>
2024-05-13 17:19:10 -0400

    Adds DeleteRoomTypeResponseDTO class to hold the infromation while performing the delete operation.

---
+++ HotelBookingAPI/DTOs/RoomTypeDTOs/DeleteRoomTypeResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelBookingAPI.DTOs.RoomTypeDTOs
+{
+    //This class is used to hold the response while Deleting a RoomType
+    public class DeleteRoomTypeResponseDTO
+    {
+        //This property is used to hold the message while performing the Delete operation
+        public string Message { get; set; }
+
+        //This property is used to hold the status of the deletion operation
+        public bool IsDeleted { get; set; }
+    }
+}
