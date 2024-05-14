14bdce6d2259026c356202d205cd87df38502a75
Karnasinh <karnasinh281@gmail.com>
2024-05-14 08:35:07 -0400

    Adds GetAllRoomTypes(bool? IsActive = null) API End point to get the all RoomTypes available in the database.

--- HotelBookingAPI/Controllers/RoomTypeController.cs
+++ HotelBookingAPI/Controllers/RoomTypeController.cs
@@ -1,9 +1,14 @@
-﻿using HotelBookingAPI.Repository;
+﻿using HotelBookingAPI.DTOs.RoomTypeDTOs;
+using HotelBookingAPI.Models;
+using HotelBookingAPI.Repository;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace HotelBookingAPI.Controllers
-{ 
+{
     //API Controller which holds all the endpoints related to RoomType CRUD operations.
+    [Route("api/[controller]")]
+    [ApiController]
     public class RoomTypeController : ControllerBase
     {
         //UserRepository instance to access the RoomType data.
@@ -17,6 +22,34 @@ namespace HotelBookingAPI.Controllers
         {
             _roomTypeRepository = roomTypeRepository;
             _logger = logger;
+        }
+
+
+        //API endpoint to get all the RoomTypes from the database based on the IsActive status.
+        [HttpGet("AllRoomTypes")]
+        public async Task<APIResponse<List<RoomTypeDTO>>> GetAllRoomTypes(bool? IsActive = null)
+        {
+            //Log the request received.
+            _logger.LogInformation($"Request Received for GetAllRoomTypes, IsActive: {IsActive}");
+
+            //Try to get all the RoomTypes.
+            try
+            {
+                //Retrieve all the RoomTypes from the database using the RoomTypeRepository's RetrieveAllRoomTypesAsync method.
+                var users = await _roomTypeRepository.RetrieveAllRoomTypesAsync(IsActive);
+
+                //Return the retrieved RoomTypes.
+                return new APIResponse<List<RoomTypeDTO>>(users, "Retrieved all Room Types Successfully.");
+            }
+            //Catch the exception if any error occurs.
+            catch (Exception ex)
+            {
+                //Log the error message.
+                _logger.LogError(ex, "Error Retriving all Room Types");
+
+                //Return the Internal server error response if any error occurs in the Try block.
+                return new APIResponse<List<RoomTypeDTO>>(HttpStatusCode.InternalServerError, "Internal server error: " + ex.Message);
+            }
         }
     }
 }
