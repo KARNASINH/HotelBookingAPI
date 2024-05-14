cd9e44c6f4b71f81c5c92034c34923e880672364
Karnasinh <karnasinh281@gmail.com>
2024-05-13 22:22:19 -0400

    Injecting RoomTypeRepository and ILogger<RoomTypeController> object to access their services.

--- HotelBookingAPI/Controllers/RoomTypeController.cs
+++ HotelBookingAPI/Controllers/RoomTypeController.cs
@@ -6,6 +6,17 @@ namespace HotelBookingAPI.Controllers
     //API Controller which holds all the endpoints related to RoomType CRUD operations.
     public class RoomTypeController : ControllerBase
     {
-       
+        //UserRepository instance to access the RoomType data.
+        private readonly RoomTypeRepository _roomTypeRepository;
+
+        //Logger instance to log the information or errors.
+        private readonly ILogger<RoomTypeController> _logger;
+
+        //Constructor to initialize the RoomTypeRepository and Logger objects.
+        public RoomTypeController(RoomTypeRepository roomTypeRepository, ILogger<RoomTypeController> logger)
+        {
+            _roomTypeRepository = roomTypeRepository;
+            _logger = logger;
+        }
     }
 }
