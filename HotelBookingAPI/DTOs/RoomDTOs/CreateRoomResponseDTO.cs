96e6b249fb960d5959f84ec58c3e327c85ca7629
Karnasinh <karnasinh281@gmail.com>
2024-05-14 20:09:32 -0400

    Adds CreateRoomResponseDTO Data Transfer Object to send response after executing the create room stored procedure.

---
+++ HotelBookingAPI/DTOs/RoomDTOs/CreateRoomResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelBookingAPI.DTOs.RoomDTOs
+{
+    //Data Transfer Object to send response after executing the create room stored procedure
+    public class CreateRoomResponseDTO
+    {
+        //Room ID
+        public int RoomID { get; set; }
+
+        //To store the error message if any
+        public string Message { get; set; }
+
+        //To store the status of the room creation
+        public bool IsCreated { get; set; }
+
+    }
+}
