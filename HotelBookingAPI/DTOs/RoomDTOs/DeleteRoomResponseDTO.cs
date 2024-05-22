66d31fc4bac52020d49a2de1699664b842f38286
Karnasinh <karnasinh281@gmail.com>
2024-05-22 16:49:09 -0400

    Adds DeleteRoomResponseDTO class  used to return a response when a room is deleted.

---
+++ HotelBookingAPI/DTOs/RoomDTOs/DeleteRoomResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelBookingAPI.DTOs.RoomDTOs
+{
+    //This class is used to return a response when a room is deleted
+    public class DeleteRoomResponseDTO
+    {
+        public string Message { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
