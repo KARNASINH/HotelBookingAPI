cef4ba68890d000b1d761b3b892fa398ba68fa1e
Karnasinh <karnasinh281@gmail.com>
2024-05-22 16:21:09 -0400

    Adds CreateRoomRequestDTO class to hold the data that is sent to the API when creating a new room

--- HotelBookingAPI/DTOs/RoomDTOs/CreateRoomRequestDTO.cs
+++ HotelBookingAPI/DTOs/RoomDTOs/CreateRoomRequestDTO.cs
@@ -2,40 +2,33 @@
 
 namespace HotelBookingAPI.DTOs.RoomDTOs
 {
-    //Data Transfer Object for creating a room
+    //This class is used to hold the data that is sent to the API when creating a new room
     public class CreateRoomRequestDTO
     {
-        //Room number
         [Required]
         [StringLength(10, ErrorMessage = "Room number must be up to 10 characters long.")]
         public string RoomNumber { get; set; }
 
-        //Room type ID, this is a foreign key
         [Required]
         [Range(1, int.MaxValue, ErrorMessage = "Invalid Room Type ID.")]
         public int RoomTypeID { get; set; }
 
-        //Price of the room
         [Required]
         [Range(typeof(decimal), "0.01", "999999.99", ErrorMessage = "Price must be between 0.01 and 999999.99.")]
         public decimal Price { get; set; }
 
-        //Bed type
         [Required]
         [StringLength(50, ErrorMessage = "Bed type must be up to 50 characters long.")]
         public string BedType { get; set; }
 
-        //View type of the room. E.g. Sea view, City view, etc.
         [Required]
         [StringLength(50, ErrorMessage = "View type must be up to 50 characters long.")]
         public string ViewType { get; set; }
 
-        //Status of the room. E.g. Available, Under Maintenance, Occupied
         [Required]
         [RegularExpression("(Available|Under Maintenance|Occupied)", ErrorMessage = "Status must be 'Available', 'Under Maintenance', or 'Occupied'.")]
         public string Status { get; set; }
 
-        //Indicates if the room is active or not
         [Required]
         public bool IsActive { get; set; }
     }
