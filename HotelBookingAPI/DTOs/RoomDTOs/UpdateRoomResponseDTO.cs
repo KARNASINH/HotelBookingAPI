ade2479c4732ec2c0927dc8464f11fa2728b8f76
Karnasinh <karnasinh281@gmail.com>
2024-05-22 16:41:17 -0400

    Adds  UpdateRoomResponseDTO class used to hold the response data while updating a room using Store procedure.

---
+++ HotelBookingAPI/DTOs/RoomDTOs/UpdateRoomResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBookingAPI.DTOs.RoomDTOs
+{
+    //This class is used to hold the response data while updating a room using Store procedure.
+    public class UpdateRoomResponseDTO
+    {
+        public int RoomId { get; set; }
+        public string Message { get; set; }
+        public bool IsUpdated { get; set; }
+    }
+}
