1b30212899d7199f74e7e7912cc9c3dc8925e1e4
Karnasinh <karnasinh281@gmail.com>
2024-05-22 16:23:57 -0400

    Adds CreateRoomResponseDTO class to hold the response data while creating a room using Store procedure.

---
+++ HotelBookingAPI/DTOs/RoomDTOs/CreateRoomResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelBookingAPI.DTOs.RoomDTOs
+{
+    //This class is used to hold the response data while creating a room using Store procedure.
+    public class CreateRoomResponseDTO
+    {
+        public int RoomID { get; set; }
+
+        public string Message { get; set; }
+
+        public int IsCreated { get; set; }
+    }
+}
