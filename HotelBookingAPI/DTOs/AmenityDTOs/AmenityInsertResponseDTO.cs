b3cae0325a40edc90ac81df2ce062e7b0b763553
Karnasinh <karnasinh281@gmail.com>
2024-05-23 19:02:19 -0400

    Adds AmenityInsertResponseDTO class which is used to hold the reponse while creating a new Amenity to the database.

---
+++ HotelBookingAPI/DTOs/AmenityDTOs/AmenityInsertResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace HotelBookingAPI.DTOs.AmenityDTOs
+{
+    //This class is used to return the response while inserting a new amenity into the database
+    public class AmenityInsertResponseDTO
+    {
+        //ID of the amenity
+        public int AmenityID { get; set; }
+
+        //Message to be returned
+        public string Message { get; set; }
+
+        //Flag to check if the amenity is created
+        public bool IsCreated { get; set; }
+    }
+}
