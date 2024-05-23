c1c71f5614bead696f77d911bf9f1dfb535cc77b
Karnasinh <karnasinh281@gmail.com>
2024-05-23 18:59:37 -0400

    Adds AmenityInsertDTO class to add new Amenity to the database.

---
+++ HotelBookingAPI/DTOs/AmenityDTOs/AmenityInsertDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.AmenityDTOs
+{
+    //This class is used to insert a new amenity into the database
+    public class AmenityInsertDTO
+    {
+        //Name of the amenity
+        [Required]
+        [StringLength(100, ErrorMessage = "Name length can't be more than 100 characters.")]
+        public string Name { get; set; }
+
+        //Description of the amenity
+        [StringLength(255, ErrorMessage = "Description length can't be more than 255 characters.")]
+        public string Description { get; set; }
+    }
+}
