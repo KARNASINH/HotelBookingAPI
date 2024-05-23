3fb445452ed9cf4eb82487acb6d5c7059bac89e0
Karnasinh <karnasinh281@gmail.com>
2024-05-22 21:33:10 -0400

    Injecting RoomRepository and ILogger<RoomController> object to access their services.

--- HotelBookingAPI/Controllers/RoomController.cs
+++ HotelBookingAPI/Controllers/RoomController.cs
@@ -6,6 +6,20 @@ namespace HotelBookingAPI.Controllers
     //API Controller which holds all the endpoints related to Room CRUD operations.
     public class RoomController : ControllerBase
     {
-        
+        //Repository object to access the methods of RoomRepository
+        private readonly RoomRepository _roomRepository;
+
+        //Logger object to log the exceptions
+        private readonly ILogger<RoomController> _logger;
+
+        //Constructor to initialize the RoomRepository and Logger objects
+        public RoomController(RoomRepository roomRepository, ILogger<RoomController> logger)
+        {
+            //Assigning the RoomRepository object to the private variable
+            _roomRepository = roomRepository;
+
+            //Assigning the Logger object to the private variable
+            _logger = logger;
+        }
     }
 }
