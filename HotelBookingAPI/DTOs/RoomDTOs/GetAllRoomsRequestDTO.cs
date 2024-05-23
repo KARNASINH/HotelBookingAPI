3569aea7ad7ce63213f16b859824ec72f4bd1249
Karnasinh <karnasinh281@gmail.com>
2024-05-23 18:46:41 -0400

    Corrected some errors in GetAllRoomsRequestDTO class.

--- HotelBookingAPI/DTOs/RoomDTOs/GetAllRoomsRequestDTO.cs
+++ HotelBookingAPI/DTOs/RoomDTOs/GetAllRoomsRequestDTO.cs
@@ -9,11 +9,11 @@ namespace HotelBookingAPI.DTOs.RoomDTOs
         //Optional filtering by RoomTypeID
         //Range attribute ensures that the RoomTypeID is a positive integer
         [Range(1, int.MaxValue, ErrorMessage = "Room Type ID must be a positive integer.")]
-        public int? RoomTypeID { get; set; };
+        public int? RoomTypeID { get; set; }
         
         //Optional filtering by Status
         //RegularExpression attribute ensures that the Status is one of the following: Available, Under Maintenance, Occupied, or All
         [RegularExpression("(Available|Under Maintenance|Occupied|All)", ErrorMessage = "Invalid status. Valid statuses are 'Available', 'Under Maintenance', 'Occupied', or 'All' for no filter.")]
-        public string? Status { get; set; };
+        public string? Status { get; set; }
     }
 }
