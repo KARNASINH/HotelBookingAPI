2c573116b698a982045bede9867ac628f3c0d6ff
Karnasinh <karnasinh281@gmail.com>
2024-05-24 13:55:08 -0400

    Adds AmenityDetailsDTO class used to return the details of the Amenity.

---
+++ HotelBookingAPI/DTOs/AmenityDTOs/AmenityDeleteResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelBookingAPI.DTOs.AmenityDTOs
+{
+    //This class is used to return a response when an amenity is deleted
+    public class AmenityDeleteResponseDTO
+    {
+        //The message to be returned
+        public string Message { get; set; }
+
+        //A boolean to indicate if the amenity was deleted
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ HotelBookingAPI/DTOs/AmenityDTOs/AmenityDetailsDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace HotelBookingAPI.DTOs.AmenityDTOs
+{
+    //This class is used to return the details of an amenity
+    public class AmenityDetailsDTO
+    {
+        //The ID of the amenity
+        public int AmenityID { get; set; }
+
+        //The name of the amenity
+        public string Name { get; set; }
+
+        //The description of the amenity
+        public string Description { get; set; }
+
+        //Flag to determine if the amenity is active
+        public bool IsActive { get; set; }
+    }
+}
