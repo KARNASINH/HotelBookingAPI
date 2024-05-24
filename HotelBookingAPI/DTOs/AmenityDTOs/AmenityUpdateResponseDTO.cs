12704dfdee4ba30a8fed09f11d852cae3ad248cd
Karnasinh <karnasinh281@gmail.com>
2024-05-24 13:45:31 -0400

    Adds AmenityUpdateResponseDTO class to hold the reponse while updating the Amenity.

---
+++ HotelBookingAPI/DTOs/AmenityDTOs/AmenityUpdateResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace HotelBookingAPI.DTOs.AmenityDTOs
+{
+    //This class is used to send the response while perforning the Amenity update operation
+    public class AmenityUpdateResponseDTO
+    {
+        //Amenity ID
+        public int AmenityID { get; set; }
+
+        //Message to be sent
+        public string Message { get; set; }
+
+        //Flag to check if the Amenity is updated
+        public bool IsUpdated { get; set; }
+    }
+}
