59a295a8e67776db5befb3ebf06f04fa913e4e67
Karnasinh <karnasinh281@gmail.com>
2024-05-24 15:18:14 -0400

    Initializing SqlConnectionFactory object using DI.

--- HotelBookingAPI/Repository/AmenityRepository.cs
+++ HotelBookingAPI/Repository/AmenityRepository.cs
@@ -1,7 +1,28 @@
-﻿namespace HotelBookingAPI.Repository
+﻿using HotelBookingAPI.Connection;
+using HotelBookingAPI.DTOs.AmenityDTOs;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace HotelBookingAPI.Repository
 {
     //This class is used to interact with the database and perform CRUD operations on the Amenity table
     public class AmenityRepository
     {
+        //Creating SqlConnectionFactory object to establish connection with Database.
+        private readonly SqlConnectionFactory _connectionFactory;
+
+
+
+
+        //Injecting SqlConnectionFactory object using DI.
+        public AmenityRepository(SqlConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+
+
+
+
     }
 }
