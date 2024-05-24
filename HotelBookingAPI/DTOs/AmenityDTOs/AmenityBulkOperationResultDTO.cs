668bbba5380515988043bd3d1dc4fb4eb73d82cc
Karnasinh <karnasinh281@gmail.com>
2024-05-24 14:08:48 -0400

    Adds AmenityStatusDTO class to upadate the Status of the Amenity.

---
+++ HotelBookingAPI/DTOs/AmenityDTOs/AmenityBulkOperationResultDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelBookingAPI.DTOs.AmenityDTOs
+{
+    //This class is ussed to perform the Bulk Insertation operation for Amenity
+    public class AmenityBulkOperationResultDTO
+    {
+        //Response message
+        public string Message { get; set; }
+
+        //Status of the operation wheather it is success or not
+        public bool IsSuccess { get; set; }
+    }
+}
---
+++ HotelBookingAPI/DTOs/AmenityDTOs/AmenityStatusDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelBookingAPI.DTOs.AmenityDTOs
+{
+    //This class is used to update the status of an amenity
+    public class AmenityStatusDTO
+    {
+        //Amenity ID
+        public int AmenityID { get; set; }
+
+        //Amenity status
+        public bool IsActive { get; set; }
+    }
+}
