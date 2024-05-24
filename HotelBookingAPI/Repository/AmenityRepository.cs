380930bae81161453d95c96480eeb67fd66e21c5
Karnasinh <karnasinh281@gmail.com>
2024-05-24 14:11:41 -0400

    Adds AmenityRepository class which holds all the CRUD and other operation methods for the Amenity.

---
+++ HotelBookingAPI/Repository/AmenityRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace HotelBookingAPI.Repository
+{
+    //This class is used to interact with the database and perform CRUD operations on the Amenity table
+    public class AmenityRepository
+    {
+    }
+}
