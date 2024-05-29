147ba3ebd36c4581f303c7e286d28bd201b44219
Karnasinh <karnasinh281@gmail.com>
2024-05-29 19:08:19 -0400

    Adds instances of AmenityRepository and ILogger using DI pattern.

--- HotelBookingAPI/Controllers/AmenityController.cs
+++ HotelBookingAPI/Controllers/AmenityController.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelBookingAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HotelBookingAPI.Controllers
 {
     //API Controller which holds all the endpoints related to Amenity CRUD operations.
     public class AmenityController : ControllerBase
     {
-        
+        //AmenityRepository instance to access the methods in the repository.
+        private readonly AmenityRepository _amenityRepository;
+
+        //ILogger instance to log the information or errors.
+        private readonly ILogger<AmenityController> _logger;
+
+        //Constructor to initialize the AmenityRepository and Logger using Dependency Injection.
+        public AmenityController(AmenityRepository amenityRepository, ILogger<AmenityController> logger)
+        {
+            _amenityRepository = amenityRepository;
+            _logger = logger;
+        }
     }
 }
