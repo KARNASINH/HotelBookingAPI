a80c221b918cda1629e97ebbcc4e09dc7d63f4b1
Karnasinh <karnasinh281@gmail.com>
2024-05-29 19:04:46 -0400

    Adds AmenityController which holds all the endpoints related to Amenity CRUD operations.

---
+++ HotelBookingAPI/Controllers/AmenityController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookingAPI.Controllers
+{
+    //API Controller which holds all the endpoints related to Amenity CRUD operations.
+    public class AmenityController : ControllerBase
+    {
+        
+    }
+}
