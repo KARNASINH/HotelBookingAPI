eaeb044367392fa6bbed46073f39ca912297e499
Karnasinh <karnasinh281@gmail.com>
2024-05-30 17:32:59 -0400

    Adds AmenityResponseDTO class which is used to get the information about the Amenity.

---
+++ HotelBookingAPI/DTOs/RoomAmenityDTOs/AmenityResponseDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace HotelBookingAPI.DTOs.RoomAmenityDTOs
+{
+    //This class is used to hold the information about the amenity.
+    public class AmenityResponseDTO
+    {
+        //The ID of the amenity.
+        public int AmenityID { get; set; }
+
+        //The name of the amenity.
+        public string Name { get; set; }
+
+        //The description of the amenity.
+        public string Description { get; set; }
+
+        //The price of the amenity.
+        public bool IsActive { get; set; }
+    }
+}
