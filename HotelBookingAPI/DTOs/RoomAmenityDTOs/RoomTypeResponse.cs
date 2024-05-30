9171ec7b57435b9ec01377eea0a6ca7c8068bf48
Karnasinh <karnasinh281@gmail.com>
2024-05-30 18:00:51 -0400

    Adds FetchRoomTypesByAmenityIdAsync(int amenityId) method to fetch all the RoomTypes based on AmenityID from the database.

--- HotelBookingAPI/DTOs/RoomAmenityDTOs/RoomTypeResponse.cs
+++ HotelBookingAPI/DTOs/RoomAmenityDTOs/RoomTypeResponse.cs
@@ -5,9 +5,19 @@ namespace HotelBookingAPI.DTOs.RoomAmenityDTOs
     //This class is used to hold the information of the RoomType.
     public class RoomTypeResponse
     {
-        [Required]
+        //This property is used to store the RoomTypeID.
         public int RoomTypeID { get; set; }
-        [Required]
-        public List<int> AmenityIDs { get; set; }
+
+        //This property is used to store the RoomTypeName.
+        public string TypeName { get; set; }
+
+        //This property is used to store the RoomTypeDescription.
+        public string Description { get; set; }
+
+        //This property is used to store the RoomTypeImage.
+        public string AccessibilityFeatures { get; set; }
+
+        //This property is used to store flag to check if the RoomType is active or not.
+        public bool IsActive { get; set; }
     }
 }
--- HotelBookingAPI/Repository/RoomAmenityRepository.cs
+++ HotelBookingAPI/Repository/RoomAmenityRepository.cs
@@ -74,7 +74,49 @@ namespace HotelBookingAPI.Repository
 
 
 
+        //This method is used to fetch all the RoomTypes based on AmenityID from the database.
+        public async Task<List<RoomTypeResponse>> FetchRoomTypesByAmenityIdAsync(int amenityId)
+        {
+            //Creating a list of RoomTypeResponse to store the response from the database.
+            var response = new List<RoomTypeResponse>();
 
+            //Creating a connection object using the CreateConnection method from the SqlConnectionFactory class.
+            using var connection = _connectionFactory.CreateConnection();
+
+            //Creating a command object to execute the stored procedure spFetchRoomTypesByAmenityID.
+            using var command = new SqlCommand("spFetchRoomTypesByAmenityID", connection)
+            {
+                //Setting the command type to stored procedure.
+                CommandType = CommandType.StoredProcedure
+            };
+
+            //Adding the AmenityID as a parameter to the command object.
+            command.Parameters.AddWithValue("@AmenityID", amenityId);
+
+            //Opening the connection.
+            await connection.OpenAsync();
+
+            //Executing the command and storing the response in a reader object.
+            using var reader = await command.ExecuteReaderAsync();
+
+            //Reading the response from the reader object and storing it in the response list.
+            while (await reader.ReadAsync())
+            {
+                //Adding the response to the response list.
+                response.Add(new RoomTypeResponse
+                {
+                    //Reading the response from the reader object and storing it in the response list.
+                    RoomTypeID = reader.GetInt32(reader.GetOrdinal("RoomTypeID")),
+                    TypeName = reader.GetString(reader.GetOrdinal("TypeName")),
+                    Description = reader.GetString(reader.GetOrdinal("Description")),
+                    AccessibilityFeatures = reader.GetString(reader.GetOrdinal("AccessibilityFeatures")),
+                    IsActive = reader.GetBoolean(reader.GetOrdinal("IsActive"))
+                });
+            }
+
+            //Returning the response list.
+            return response;
+        }
 
 
     }
