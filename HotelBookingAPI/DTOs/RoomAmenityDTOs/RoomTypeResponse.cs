c1156826ced42fb86c4e238d5d1586fbf4fb3861
Karnasinh <karnasinh281@gmail.com>
2024-05-30 17:35:24 -0400

    Adds RoomTypeResponse class used to hold the information of the RoomType.

---
+++ HotelBookingAPI/DTOs/RoomAmenityDTOs/RoomTypeResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.RoomAmenityDTOs
+{
+    //This class is used to hold the information of the RoomType.
+    public class RoomTypeResponse
+    {
+        [Required]
+        public int RoomTypeID { get; set; }
+        [Required]
+        public List<int> AmenityIDs { get; set; }
+    }
+}
