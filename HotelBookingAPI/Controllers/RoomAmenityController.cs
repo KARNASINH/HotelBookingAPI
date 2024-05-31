ab20bf4a85b0c4649f44deec6a85be66d4aa469b
Karnasinh <karnasinh281@gmail.com>
2024-05-30 22:28:04 -0400

    Adds Privates fields for RoomAmenityRepository and ILogger. Also, adds constructor to initiliaze both fields using DI pattern.

--- HotelBookingAPI/Controllers/RoomAmenityController.cs
+++ HotelBookingAPI/Controllers/RoomAmenityController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelBookingAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HotelBookingAPI.Controllers
 {
     //This controller is responsible for handling requests related to room amenities.
     public class RoomAmenityController : ControllerBase
     {
-        
+        //Created private fields for RoomAmenityRepository and ILogger.
+        private readonly RoomAmenityRepository _roomAmenityRepository;
+        private readonly ILogger<RoomAmenityController> _logger;
+
+        //Constructor to initialize the RoomAmenityRepository and ILogger.
+        public RoomAmenityController(RoomAmenityRepository roomAmenityRepository, ILogger<RoomAmenityController> logger)
+        {
+            _roomAmenityRepository = roomAmenityRepository;
+            _logger = logger;
+        }
+
     }
 }
