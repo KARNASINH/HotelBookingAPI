f93d4654a3334c47a69d8676e748f9cab18b3115
Karnasinh <karnasinh281@gmail.com>
2024-06-01 19:15:17 -0400

    Adds PriceRangeHotelSearchRequestDTO class to hold the Price data which is used for hotel search functionality.

---
+++ HotelBookingAPI/DTOs/HotelSearchDTOs/PriceRangeHotelSearchRequestDTO.cs
@@ -0,0 +1,20 @@
+﻿using HotelBookingAPI.CustomValidator;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.HotelSearchDTOs
+{
+    //This class is used to hold the data that will be used to search for hotels by price range
+    public class PriceRangeHotelSearchRequestDTO
+    {
+        //This is the minimum price that the user wants to search for
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Minimum price must be greater than or equal to 0.")]
+        public decimal MinPrice { get; set; }
+
+        //This is the maximum price that the user wants to search for
+        [Required]
+        [Range(1, double.MaxValue, ErrorMessage = "Maximum price must be greater than 0.")]
+        [PriceRangeValidation("MinPrice", "MaxPrice")]
+        public decimal MaxPrice { get; set; }
+    }
+}
