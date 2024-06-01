856259c13e2c9798038a531276c3580a46c18f1d
Karnasinh <karnasinh281@gmail.com>
2024-06-01 19:59:09 -0400

    Adds RoomDetailsWithAmenitiesSearchDTO class used to return the data for Room, RoomType and Amenities.

---
+++ HotelBookingAPI/DTOs/HotelSearchDTOs/AmenitySearchDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelBookingAPI.DTOs.HotelSearchDTOs
+{ 
+    //This class is used to hold the information of the Amenity.
+    public class AmenitySearchDTO
+    {
+        //This is the ID of the Amenity.
+        public int AmenityID { get; set; }
+
+        //This is the Name of the Amenity.
+        public string Name { get; set; }
+
+        //Description of the Amenity.
+        public string Description { get; set; }
+    }
+}
+
---
+++ HotelBookingAPI/DTOs/HotelSearchDTOs/RoomDetailsWithAmenitiesSearchDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelBookingAPI.DTOs.HotelSearchDTOs
+{
+    // This class is used to return the data for Room, RoomType and Amenities.
+    public class RoomDetailsWithAmenitiesSearchDTO
+    {
+        //Room details along with RoomType details.
+        public RoomSearchDTO Room { get; set; }
+
+        //List of Amenities for the Room.
+        public List<AmenitySearchDTO> Amenities { get; set; }
+    }
+}
