cf96a1df0b91f2864390d8268384605f20cf3468
Karnasinh <karnasinh281@gmail.com>
2024-06-01 19:06:57 -0400

    Adds AvailabilityHotelSearchRequestDTO class to hold the checkin and checkout date for the searching functionality.

---
+++ HotelBookingAPI/DTOs/HotelSearchDTOs/AvailabilityHotelSearchRequestDTO.cs
@@ -0,0 +1,22 @@
+﻿using HotelBookingAPI.CustomValidator;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.HotelSearchDTOs
+{
+    //This class holds the data which are required to search the Hotel based on the CheckIn and CheckOut dates.
+    public class AvailabilityHotelSearchRequestDTO
+    {
+        //Checkin date for the hotel search
+        [Required]
+        [DataType(DataType.Date)]
+        [FutureDateValidation(ErrorMessage = "Check-in date must be in the future.")]
+        public DateTime CheckInDate { get; set; }
+     
+
+        [Required]
+        [DataType(DataType.Date)]
+        [FutureDateValidation(ErrorMessage = "Check-out date must be in the future.")]
+        [DateGreaterThanValidation("CheckInDate", ErrorMessage = "Check-out date must be after check-in date.")]
+        public DateTime CheckOutDate { get; set; }
+    }
+}
