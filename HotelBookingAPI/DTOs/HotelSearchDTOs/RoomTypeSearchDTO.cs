79687e988d3e7bdc758c6eeff4344f05d777b559
Karnasinh <karnasinh281@gmail.com>
2024-06-01 19:45:40 -0400

    Adds RoomTypeSearchDTO class used to hold the infromation that is returned when a room type is searched for.

---
+++ HotelBookingAPI/DTOs/HotelSearchDTOs/RoomTypeSearchDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace HotelBookingAPI.DTOs.HotelSearchDTOs
+{
+    //This class is used to hold the infromation that is returned when a room type is searched for
+    public class RoomTypeSearchDTO
+    {
+        //The ID of the room type
+        public int RoomTypeID { get; set; }
+
+        //The name of the room type
+        public string TypeName { get; set; }
+
+        //The Accessibility features of the room type
+        public string AccessibilityFeatures { get; set; }
+
+        //The description of the room type
+        public string Description { get; set; }
+    }
+}
