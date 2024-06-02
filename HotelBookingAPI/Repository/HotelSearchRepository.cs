a2d2e582549b0806567d75bd0ab3fb4bf7990e01
Karnasinh <karnasinh281@gmail.com>
2024-06-02 10:06:18 -0400

    Adds SearchByViewTypeAsync(string viewType) method to fetch the Room details from the database based on the ViewType provided by the user.

--- HotelBookingAPI/Repository/HotelSearchRepository.cs
+++ HotelBookingAPI/Repository/HotelSearchRepository.cs
@@ -183,5 +183,47 @@ namespace HotelBookingAPI.Repository
             return rooms;
         }
 
+
+
+
+
+        //This method is used to fetch the Room details from the database based on the ViewType provided by the user.
+        public async Task<List<RoomSearchDTO>> SearchByViewTypeAsync(string viewType)
+        {
+            //List to store the Room fetched from the database.
+            var rooms = new List<RoomSearchDTO>();
+
+            //Creating a connection object using the SqlConnectionFactory object.
+            using (var connection = _connectionFactory.CreateConnection())
+            {
+                //Creating a SqlCommand object to execute the stored procedure spSearchByViewType.
+                using (var command = new SqlCommand("spSearchByViewType", connection))
+                {
+                    //Setting the command type to stored procedure.
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    //Adding the parameters to the stored procedure.
+                    command.Parameters.Add(new SqlParameter("@ViewType", viewType));
+
+                    //Opening the connection.
+                    connection.Open();
+
+                    //Executing the command and fetching the data using ExecuteReaderAsync method.
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        //Reading the data from the reader object.
+                        while (await reader.ReadAsync())
+                        {
+                            //Calling helper method to get the RoomSearchDTO object and adding it to the list.
+                            rooms.Add(CreateRoomSearchDTO(reader));
+                        }
+                    }
+                }
+            }
+
+            //Returning the list of RoomSearchDTO objects.
+            return rooms;
+        }
+
     }
 }
