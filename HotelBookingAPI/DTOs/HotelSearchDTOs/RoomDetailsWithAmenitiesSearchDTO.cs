d22df53b70b18334e13c71b0daf0e10fc3b9ea44
Karnasinh <karnasinh281@gmail.com>
2024-06-02 08:39:13 -0400

    Adds HotelSearchRepository class is used to interact with the database and executes the Search queries to get the various details of the Hotel Rooms.

--- HotelBookingAPI/DTOs/HotelSearchDTOs/RoomDetailsWithAmenitiesSearchDTO.cs
+++ HotelBookingAPI/DTOs/HotelSearchDTOs/RoomDetailsWithAmenitiesSearchDTO.cs
@@ -1,6 +1,6 @@
 ﻿namespace HotelBookingAPI.DTOs.HotelSearchDTOs
 {
-    // This class is used to return the data for Room, RoomType and Amenities.
+    //This class is used to return the data for Room, RoomType and Amenities.
     public class RoomDetailsWithAmenitiesSearchDTO
     {
         //Room details along with RoomType details.
---
+++ HotelBookingAPI/Repository/HotelSearchRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace HotelBookingAPI.Repository
+{
+    //This class is used to interact with the database and executes the Search queries to get the various details of the Hotel Rooms.
+    public class HotelSearchRepository
+    {
+    }
+}
