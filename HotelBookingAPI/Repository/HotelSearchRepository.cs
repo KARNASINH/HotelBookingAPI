df67fe14a7a53648dca435888ac1ae18a912f3af
Karnasinh <karnasinh281@gmail.com>
2024-06-02 09:26:06 -0400

    Adds SearchByAvailabilityAsync(DateTime checkInDate, DateTime checkOutDate) method which return the list of Rooms based on the given CheckIn and CheckOut date.

--- HotelBookingAPI/Repository/HotelSearchRepository.cs
+++ HotelBookingAPI/Repository/HotelSearchRepository.cs
@@ -1,4 +1,7 @@
 ﻿using HotelBookingAPI.Connection;
+using HotelBookingAPI.DTOs.HotelSearchDTOs;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace HotelBookingAPI.Repository
 {
@@ -8,6 +11,11 @@ namespace HotelBookingAPI.Repository
         //SqlConnectionFactory object to get the connection object.
         private readonly SqlConnectionFactory _connectionFactory;
 
+
+
+
+
+
         //Constructor to initialize the SqlConnectionFactory object using dependency injection.
         public HotelSearchRepository(SqlConnectionFactory connectionFactory)
         {
@@ -15,6 +23,76 @@ namespace HotelBookingAPI.Repository
             _connectionFactory = connectionFactory;
         }
 
+
+
+
+
+
+        //This method is used to fetch the Room details from the database based on the CheckIn and CheckOut dates.
+        public async Task<List<RoomSearchDTO>> SearchByAvailabilityAsync(DateTime checkInDate, DateTime checkOutDate)
+        {
+            //List to store the Room fetched from the database.
+            var rooms = new List<RoomSearchDTO>();
+
+            //Creating a connection object using the SqlConnectionFactory object.
+            using (var connection = _connectionFactory.CreateConnection())
+            {
+                //Creating a SqlCommand object to execute the stored procedure spSearchByAvailability.
+                using (var command = new SqlCommand("spSearchByAvailability", connection))
+                {
+                    //Setting the command type to stored procedure.
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    //Adding the parameters to the stored procedure.
+                    command.Parameters.Add(new SqlParameter("@CheckInDate", checkInDate));
+                    command.Parameters.Add(new SqlParameter("@CheckOutDate", checkOutDate));
+
+                    //Opening the connection.
+                    connection.Open();
+
+                    //Executing the command and fetching the data using ExecuteReaderAsync method.
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        //Reading the data from the reader object.
+                        while (await reader.ReadAsync())
+                        {
+                            //Calling helper method to create the RoomSearchDTO object and adding it to the list.
+                            rooms.Add(CreateRoomSearchDTO(reader));
+                        }
+                    }
+                }
+            }
+
+            //Returning the list of RoomSearchDTO objects.
+            return rooms;
+        }
+
+
+
+
+
+
+        //This would be a Helper method which takes SqlDataReader object as input and returns the RoomSearchDTO object.
+        //In all the methods in the Repository class, we would be using this method to create and to return the RoomSearchDTO object.
+        private RoomSearchDTO CreateRoomSearchDTO(SqlDataReader reader)
+        {
+            return new RoomSearchDTO
+            {
+                RoomID = reader.GetInt32(reader.GetOrdinal("RoomID")),
+                RoomNumber = reader.GetString(reader.GetOrdinal("RoomNumber")),
+                Price = reader.GetDecimal(reader.GetOrdinal("Price")),
+                BedType = reader.GetString(reader.GetOrdinal("BedType")),
+                ViewType = reader.GetString(reader.GetOrdinal("ViewType")),
+                Status = reader.GetString(reader.GetOrdinal("Status")),
+                RoomType = new RoomTypeSearchDTO
+                {
+                    RoomTypeID = reader.GetInt32(reader.GetOrdinal("RoomTypeID")),
+                    TypeName = reader.GetString(reader.GetOrdinal("TypeName")),
+                    AccessibilityFeatures = reader.GetString(reader.GetOrdinal("AccessibilityFeatures")),
+                    Description = reader.GetString(reader.GetOrdinal("Description"))
+                }
+            };
+        }
 
     }
 }
