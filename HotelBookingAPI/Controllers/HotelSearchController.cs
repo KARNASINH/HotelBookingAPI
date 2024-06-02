5f1d8bb6705673002645e8827e5f2bc7c27457ba
Karnasinh <karnasinh281@gmail.com>
2024-06-02 13:39:16 -0400

    Adds HotelSearchRepository and  ILogger<HotelSearchController> object using DI pattern.

--- HotelBookingAPI/Controllers/HotelSearchController.cs
+++ HotelBookingAPI/Controllers/HotelSearchController.cs
@@ -1,10 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelBookingAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HotelBookingAPI.Controllers
 {
     //API Controller which holds all the endpoints related to Room Search operations.
+
+    [ApiController]
+    [Route("[controller]")]
     public class HotelSearchController : ControllerBase
     {
-        
+        //Repository object to access the methods of HotelSearchRepository.
+        private readonly HotelSearchRepository _hotelSearchRepository;
+
+        //Logger object to log the information or errors.
+        private readonly ILogger<HotelSearchController> _logger;
+
+        //Constructor to initialize the Repository and Logger objects.
+        public HotelSearchController(HotelSearchRepository hotelSearchRepository, ILogger<HotelSearchController> logger)
+        {
+            //Initialize the Repository object.
+            _hotelSearchRepository = hotelSearchRepository;
+            //Initialize the Logger object.
+            _logger = logger;
+        }
+
     }
 }
