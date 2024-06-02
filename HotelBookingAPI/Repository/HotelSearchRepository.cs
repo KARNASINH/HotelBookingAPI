fcdae26107428ef9761a8b4df20c79f233908d10
Karnasinh <karnasinh281@gmail.com>
2024-06-02 08:42:39 -0400

    Adds SqlConnectionFactory object and initiliazed it using DI via Constructor.

--- HotelBookingAPI/Repository/HotelSearchRepository.cs
+++ HotelBookingAPI/Repository/HotelSearchRepository.cs
@@ -1,7 +1,20 @@
-﻿namespace HotelBookingAPI.Repository
+﻿using HotelBookingAPI.Connection;
+
+namespace HotelBookingAPI.Repository
 {
     //This class is used to interact with the database and executes the Search queries to get the various details of the Hotel Rooms.
     public class HotelSearchRepository
     {
+        //SqlConnectionFactory object to get the connection object.
+        private readonly SqlConnectionFactory _connectionFactory;
+
+        //Constructor to initialize the SqlConnectionFactory object using dependency injection.
+        public HotelSearchRepository(SqlConnectionFactory connectionFactory)
+        {
+            //Assigning the SqlConnectionFactory object to the private variable.
+            _connectionFactory = connectionFactory;
+        }
+
+
     }
 }
