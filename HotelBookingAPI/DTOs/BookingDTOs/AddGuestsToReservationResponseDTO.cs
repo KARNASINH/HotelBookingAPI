64ed74e74a79c046bcd38344b7d370112216e79c
Karnasinh <karnasinh281@gmail.com>
2024-06-12 15:20:16 -0400

    Adds AddGuestsToReservationResponseDTO class to hold the response while adding guests into for the reservation.

---
+++ HotelBookingAPI/DTOs/BookingDTOs/AddGuestsToReservationResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelBookingAPI.DTOs.BookingDTOs
+{
+    //This class is used to return the response of adding guests to a reservation
+    public class AddGuestsToReservationResponseDTO
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+    }
+}
