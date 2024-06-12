c5651d5ff048e0a35b3f708ba85e3e2c4f6e5ca7
Karnasinh <karnasinh281@gmail.com>
2024-06-12 16:34:51 -0400

    Adds CalculateRoomCostsAsync(RoomCostsDTO model) method to get the Rooms list along with the cost.

--- HotelBookingAPI/Repository/ReservationRepository.cs
+++ HotelBookingAPI/Repository/ReservationRepository.cs
@@ -1,4 +1,7 @@
 ﻿using HotelBookingAPI.Connection;
+using HotelBookingAPI.DTOs.BookingDTOs;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace HotelBookingAPI.Repository
 {
@@ -8,10 +11,103 @@ namespace HotelBookingAPI.Repository
         //Private field of SqlConnectionFactory class to establish the connection with Database.
         private readonly SqlConnectionFactory _connectionFactory;
 
+
+
+
+
+
         //Injecting SqlConnectionFactory object using DI.
         public ReservationRepository(SqlConnectionFactory connectionFactory)
         {
             _connectionFactory = connectionFactory;
+        }
+
+
+
+
+
+
+        //This method is used to calculate the room costs based on the check-in and check-out dates and the room IDs.
+        public async Task<RoomCostsResponseDTO> CalculateRoomCostsAsync(RoomCostsDTO model)
+        {
+            //Creating an object of RoomCostsResponseDTO class to hold the response.
+            RoomCostsResponseDTO roomCostsResponseDTO = new RoomCostsResponseDTO();
+
+            //Try block to execute the code and catch the exceptions if any.
+            try
+            {
+                //Creating a connection object using the CreateConnection method of SqlConnectionFactory class.
+                using var connection = _connectionFactory.CreateConnection();
+
+                //Creating a command object to execute the stored procedure.
+                using var command = new SqlCommand("spCalculateRoomCosts", connection);
+
+                //Setting the command type to stored procedure.
+                command.CommandType = CommandType.StoredProcedure;
+
+                //Setting up the parameters for the stored procedure.
+                command.Parameters.AddWithValue("@CheckInDate", model.CheckInDate);
+                command.Parameters.AddWithValue("@CheckOutDate", model.CheckOutDate);
+
+
+                //Creating a DataTable object to hold the RoomIDs.
+                var table = new DataTable();
+
+                //Adding the column to the DataTable.
+                table.Columns.Add("RoomID", typeof(int));
+
+                //Adding the RoomIDs to the DataTable.
+                model.RoomIDs.ForEach(id => table.Rows.Add(id));
+
+                //Adding the DataTable as a parameter to the stored procedure.
+                command.Parameters.AddWithValue("@RoomIDs", table).SqlDbType = SqlDbType.Structured;
+                
+                //Adding the output parameters to the stored procedure.
+                command.Parameters.Add("@Amount", SqlDbType.Decimal).Direction = ParameterDirection.Output;
+                command.Parameters.Add("@GST", SqlDbType.Decimal).Direction = ParameterDirection.Output;
+                command.Parameters.Add("@TotalAmount", SqlDbType.Decimal).Direction = ParameterDirection.Output;
+
+                //Opening the connection.
+                await connection.OpenAsync();
+
+                //Executing the command and storing the result in a reader object.
+                using var reader = await command.ExecuteReaderAsync();
+
+                //Reading the data from the reader object.
+                while (reader.Read())
+                {
+                    //Adding the room details to the RoomDetails list of RoomCostsResponseDTO object.
+                    roomCostsResponseDTO.RoomDetails.Add(new RoomCostDetailDTO
+                    {
+                        //Reading the data from the reader object and adding it to the RoomCostDetailDTO object.
+                        RoomID = reader.GetInt32(reader.GetOrdinal("RoomID")),
+                        RoomNumber = reader.GetString(reader.GetOrdinal("RoomNumber")),
+                        RoomPrice = reader.GetDecimal(reader.GetOrdinal("RoomPrice")),
+                        TotalPrice = reader.GetDecimal(reader.GetOrdinal("TotalPrice")),
+                        NumberOfNights = reader.GetInt32(reader.GetOrdinal("NumberOfNights"))
+                    });
+                }
+
+                // Ensuring the reader is closed before accessing output parameters
+                await reader.CloseAsync();
+
+                //Setting the output parameters to the RoomCostsResponseDTO object.
+                roomCostsResponseDTO.Amount = (decimal)command.Parameters["@Amount"].Value;
+                roomCostsResponseDTO.GST = (decimal)command.Parameters["@GST"].Value;
+                roomCostsResponseDTO.TotalAmount = (decimal)command.Parameters["@TotalAmount"].Value;
+                roomCostsResponseDTO.Status = true;
+                roomCostsResponseDTO.Message = "Sucess";
+            }
+            //Catch block to catch the exceptions if any.
+            catch (Exception ex)
+            {
+                //Setting the status to false and the message to the exception message.
+                roomCostsResponseDTO.Status = false;
+                roomCostsResponseDTO.Message = ex.Message;
+            }
+
+            //Returning the RoomCostsResponseDTO object.
+            return roomCostsResponseDTO;
         }
     }
 }
