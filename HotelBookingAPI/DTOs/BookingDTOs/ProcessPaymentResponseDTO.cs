03ae02f8078c53391ef84e1510d4a1ec8dd88049
Karnasinh <karnasinh281@gmail.com>
2024-06-12 15:25:41 -0400

    Adds ProcessPaymentResponseDTO class to hold the response wheather the payment is successful or not.

---
+++ HotelBookingAPI/DTOs/BookingDTOs/ProcessPaymentResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBookingAPI.DTOs.BookingDTOs
+{
+    //THis class is used to hold the response wheather the payment is successful or not
+    public class ProcessPaymentResponseDTO
+    {
+        public int PaymentID { get; set; }
+        public bool Status { get; set; }
+        public string Message { get; set; }
+    }
+}
