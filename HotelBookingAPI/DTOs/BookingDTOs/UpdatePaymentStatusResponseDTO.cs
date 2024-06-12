57f7dd2a11816252a3b8453ad32487c13499ab69
Karnasinh <karnasinh281@gmail.com>
2024-06-12 15:38:16 -0400

    Adds UpdatePaymentStatusResponseDTO class to hold the response while updating the Payment Status.

---
+++ HotelBookingAPI/DTOs/BookingDTOs/UpdatePaymentStatusResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelBookingAPI.DTOs.BookingDTOs
+{
+    //This class hold the properties required to hold the reponse while updating the payment status
+    public class UpdatePaymentStatusResponseDTO
+    {
+        public bool Status { get; set; }
+
+        //Message to be displayed after updating the payment status
+        public string Message { get; set; }
+    }
+}
