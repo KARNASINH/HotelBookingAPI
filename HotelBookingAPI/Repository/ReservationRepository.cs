dbe1d5cbc9e9c616fc9f10175cdd8be34d8e3496
Karnasinh <karnasinh281@gmail.com>
2024-06-12 15:51:04 -0400

    Adds object of SqlConnectionFactory using DI pattern.

--- HotelBookingAPI/Repository/ReservationRepository.cs
+++ HotelBookingAPI/Repository/ReservationRepository.cs
@@ -5,6 +5,13 @@ namespace HotelBookingAPI.Repository
     //This class is used to perform CRUD operations for the Reservations.
     public class ReservationRepository
     {
-        
+        //Private field of SqlConnectionFactory class to establish the connection with Database.
+        private readonly SqlConnectionFactory _connectionFactory;
+
+        //Injecting SqlConnectionFactory object using DI.
+        public ReservationRepository(SqlConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
     }
 }
