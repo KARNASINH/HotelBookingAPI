7d94f675568c468e4c5d10235b9738c0a730a210
Karnasinh <karnasinh281@gmail.com>
2024-06-12 15:14:09 -0400

    Adds AddGuestsToReservationDTO class to hold the the information for adding guests to a reservation.

---
+++ HotelBookingAPI/DTOs/BookingDTOs/AddGuestsToReservationDTO.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.BookingDTOs
+{
+    //This class is used to hold the information for adding guests to a reservation
+    public class AddGuestsToReservationDTO
+    {
+        [Required]
+        public int UserID { get; set; }
+        [Required]
+        public int ReservationID { get; set; }
+        [Required]
+        
+        //Guest details inbcluding room association
+        [MinLength(1, ErrorMessage = "At least one guest detail must be provided.")]
+        public List<GuestDetail> GuestDetails { get; set; }
+    }
+    public class GuestDetail
+    {
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Phone]
+        public string Phone { get; set; }
+        [Required]
+        public string AgeGroup { get; set; }
+        public string Address { get; set; }
+        [Required]
+        public int CountryId { get; set; }
+        [Required]
+        public int StateId { get; set; }
+
+        //Room Id associated with the guest
+        [Required]
+        public int RoomID { get; set; }
+    }
+}
---
+++ HotelBookingAPI/DTOs/BookingDTOs/CreateReservationResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace HotelBookingAPI.DTOs.BookingDTOs
+{
+    //This class is used to hold the reponse while creating a reservation
+    public class CreateReservationResponseDTO
+    {
+        //Reservation ID
+        public int ReservationID { get; set; }
+
+        //Status of the response
+        public bool Status { get; set; }
+
+        //Message of the response
+        public string Message { get; set; }
+    }
+}
