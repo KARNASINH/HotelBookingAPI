1004934fa0b6ead983b4d88f7049a2e272b45716
Karnasinh <karnasinh281@gmail.com>
2024-06-12 15:23:33 -0400

    Adds ProcessPaymentDTO class to add the Payment details into the Database.

---
+++ HotelBookingAPI/DTOs/BookingDTOs/ProcessPaymentDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.BookingDTOs
+{
+    //This class is used to process payment for a reservation
+    public class ProcessPaymentDTO
+    {
+        [Required]
+        public int ReservationID { get; set; }
+        
+        //The total amount to be paid for the reservation and it should be greater than zero
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Total amount must be greater than zero.")]
+        public decimal TotalAmount { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        public string PaymentMethod { get; set; }
+    }
+}
