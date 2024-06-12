58e3a63c98a191fc7bb741934e5f352e30ba961e
Karnasinh <karnasinh281@gmail.com>
2024-06-12 11:53:42 -0400

    Adds RoomCostsDTO input class to get the costs of the rooms for the given check-in and check-out date.

---
+++ HotelBookingAPI/DTOs/BookingDTOs/RoomCostsDTO.cs
@@ -0,0 +1,23 @@
+﻿using HotelBookingAPI.CustomValidator;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.BookingDTOs
+{
+    //Input Class to get the costs of rooms for the given check-in and check-out dates
+    public class RoomCostsDTO
+    {
+        [Required]
+        public List<int> RoomIDs { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [FutureDateValidation(ErrorMessage = "Check-in date must be in the future date.")]
+        public DateTime CheckInDate { get; set; }
+        
+        [Required]
+        [DataType(DataType.Date)]
+        [FutureDateValidation(ErrorMessage = "Check-out date must be in the future.")]
+        [DateGreaterThanValidation("CheckInDate", ErrorMessage = "Check-out date must be after check-in date.")]
+        public DateTime CheckOutDate { get; set; }
+    }
+}
