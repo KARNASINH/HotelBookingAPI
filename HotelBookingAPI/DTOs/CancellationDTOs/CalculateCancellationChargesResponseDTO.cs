8def00a9a84963e3b5642ee86b79cf867a1cd8b4
Karnasinh <karnasinh281@gmail.com>
2024-06-16 19:42:08 -0400

    Adds CalculateCancellationChargesResponseDTO class to hold the response while calculating the cancellation charges.

---
+++ HotelBookingAPI/DTOs/CancellationDTOs/CalculateCancellationChargesResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelBookingAPI.DTOs.CancellationDTOs
+{
+    //This class is used to hold the response while calculating the cancellation charges.
+    public class CalculateCancellationChargesResponseDTO
+    {
+        public decimal TotalCost { get; set; }
+        public decimal CancellationCharge { get; set; }
+        public decimal CancellationPercentage { get; set; }
+        public string PolicyDescription { get; set; }
+        public bool Status { get; set; }
+        public string Message { get; set; }
+    }
+}
