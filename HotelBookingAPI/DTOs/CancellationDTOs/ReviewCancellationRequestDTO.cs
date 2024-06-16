07bd5e549ab96e17c665d669ddd419cf17421d33
Karnasinh <karnasinh281@gmail.com>
2024-06-16 19:44:21 -0400

    Adds ReviewCancellationRequestDTO class to hold the request for reviewing a cancellation request.

---
+++ HotelBookingAPI/DTOs/CancellationDTOs/ReviewCancellationRequestDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.CancellationDTOs
+{
+    //This class is used to hold the request for reviewing a cancellation request.
+    public class ReviewCancellationRequestDTO
+    {
+        [Required(ErrorMessage = "CancellationRequestID is required.")]
+        public int CancellationRequestID { get; set; }
+        
+        [Required(ErrorMessage = "AdminUserID is required.")]
+        public int AdminUserID { get; set; }
+        
+        [Required(ErrorMessage = "ApprovalStatus is required.")]
+        [StringLength(50, ErrorMessage = "ApprovalStatus length cannot exceed 50 characters.")]
+        public string ApprovalStatus { get; set; }
+    }
+}
