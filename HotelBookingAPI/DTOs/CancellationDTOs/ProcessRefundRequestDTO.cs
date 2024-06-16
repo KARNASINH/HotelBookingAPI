03f440813323eca4e6a2c6be7821776c346cc683
Karnasinh <karnasinh281@gmail.com>
2024-06-16 19:52:29 -0400

    Adds ProcessRefundRequestDTO class to hold the information required to process a refund.

---
+++ HotelBookingAPI/DTOs/CancellationDTOs/ProcessRefundRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.CancellationDTOs
+{
+    //This class is used to hold the information required to process a refund
+    public class ProcessRefundRequestDTO
+    {
+        [Required(ErrorMessage = "CancellationRequestID is required.")]
+        public int CancellationRequestID { get; set; }
+        
+        [Required(ErrorMessage = "ProcessedByUserID is required.")]
+        public int ProcessedByUserID { get; set; }
+        
+        [Required(ErrorMessage = "RefundMethodID is required.")]
+        public int RefundMethodID { get; set; }
+    }
+}
