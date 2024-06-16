e8c231ca60576ce3e660c761b3115eac4e9dc0bf
Karnasinh <karnasinh281@gmail.com>
2024-06-16 19:33:05 -0400

    Adds AllCancellationsRequestDTO class to hold the properties required for fetching all Cancellation information based on the provided optional parameters.

---
+++ HotelBookingAPI/DTOs/CancellationDTOs/AllCancellationsRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.CancellationDTOs
+{
+    //This class is used to hold the properties required for fetching all Cancellation information based on the provided optional parameters.
+    public class AllCancellationsRequestDTO
+    {
+        [StringLength(50, ErrorMessage = "Status length cannot exceed 50 characters.")]
+        public string? Status { get; set; }
+        
+        [DataType(DataType.Date)]
+        public DateTime? DateFrom { get; set; }
+        
+        [DataType(DataType.Date)]
+        public DateTime? DateTo { get; set; }
+    }
+}
