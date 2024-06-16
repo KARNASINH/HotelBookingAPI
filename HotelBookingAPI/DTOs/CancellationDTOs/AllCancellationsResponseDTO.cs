6b98997294f3419c4e043dfde02d4579006261bc
Karnasinh <karnasinh281@gmail.com>
2024-06-16 19:35:52 -0400

    Adds AllCancellationsResponseDTO and CancellationRequestDTO classes to hold the response for fetching all Cancellation information.

---
+++ HotelBookingAPI/DTOs/CancellationDTOs/AllCancellationsResponseDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace HotelBookingAPI.DTOs.CancellationDTOs
+{
+    //This class is used to hold the response for fetching all Cancellation information.
+    public class AllCancellationsResponseDTO
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+        public List<CancellationRequestDTO> Cancellations { get; set; }
+    }
+
+    //This class holds the cancellation request details
+    public class CancellationRequestDTO
+    {
+        public int CancellationRequestID { get; set; }
+        public int ReservationID { get; set; }
+        public int UserID { get; set; }
+        public string CancellationType { get; set; }
+        public DateTime RequestedOn { get; set; }
+        public string Status { get; set; }
+    }
+}
