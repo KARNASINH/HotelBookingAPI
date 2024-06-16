52364c020abd81f417f1238bad61892dca5a2f16
Karnasinh <karnasinh281@gmail.com>
2024-06-16 19:28:35 -0400

    Adds CancellationPoliciesResponseDTO and CancellationPolicyDTO class to hold the the response of the cancellation policies and holds the cancellation policy details respectively.

---
+++ HotelBookingAPI/DTOs/CancellationDTOs/CancellationPoliciesResponseDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace HotelBookingAPI.DTOs.CancellationDTOs
+{
+    //This class is used to return the response of the cancellation policies
+    public class CancellationPoliciesResponseDTO
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+        public List<CancellationPolicyDTO> Policies { get; set; }
+    }
+
+    //This class holds the cancellation policy details
+    public class CancellationPolicyDTO
+    {
+        public int PolicyID { get; set; }
+        public string Description { get; set; }
+        public decimal CancellationChargePercentage { get; set; }
+        public decimal MinimumCharge { get; set; }
+        public DateTime EffectiveFromDate { get; set; }
+        public DateTime EffectiveToDate { get; set; }
+    }
+}
