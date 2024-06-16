d1122067040e107ff728e735432591b15498475b
Karnasinh <karnasinh281@gmail.com>
2024-06-16 19:54:49 -0400

    Adds ProcessRefundResponseDTO class to hold the information of the refund process response.

---
+++ HotelBookingAPI/DTOs/CancellationDTOs/ProcessRefundResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBookingAPI.DTOs.CancellationDTOs
+{
+    //This class is used to hold the information of the refund process response
+    public class ProcessRefundResponseDTO
+    {
+        public int RefundID { get; set; }
+        public bool Status { get; set; }
+        public string Message { get; set; }
+    }
+}
