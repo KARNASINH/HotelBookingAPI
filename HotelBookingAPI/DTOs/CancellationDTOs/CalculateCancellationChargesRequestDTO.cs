22566f39d581c7ebe279627abe2a41ceb94e1673
Karnasinh <karnasinh281@gmail.com>
2024-06-16 19:40:51 -0400

    Adds CalculateCancellationChargesRequestDTO class to hold the request for calculating the cancellation charges.

---
+++ HotelBookingAPI/DTOs/CancellationDTOs/CalculateCancellationChargesRequestDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.DTOs.CancellationDTOs
+{
+    //This class is used to hold the request for calculating the cancellation charges
+    public class CalculateCancellationChargesRequestDTO
+    {
+        [Required(ErrorMessage = "ReservationID is required.")]
+        public int ReservationID { get; set; }
+        
+        [Required(ErrorMessage = "RoomsCancelled list cannot be empty.")]
+        [MinLength(1, ErrorMessage = "At least one room must be cancelled.")]
+        public List<int> RoomsCancelled { get; set; }
+    }
+}
