248876d81327dc688b3eb280af3080af4cc26422
Karnasinh <karnasinh281@gmail.com>
2024-06-16 19:31:11 -0400

    Adds CreateCancellationResponseDTO class to hold the response for creating a cancellation request.

---
+++ HotelBookingAPI/DTOs/CancellationDTOs/CreateCancellationResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBookingAPI.DTOs.CancellationDTOs
+{
+    //This class is used to hold the response for creating a cancellation request
+    public class CreateCancellationResponseDTO
+    {
+        public int CancellationId { get; set; }
+        public bool Status { get; set; }
+        public string Message { get; set; }
+    }
+}
