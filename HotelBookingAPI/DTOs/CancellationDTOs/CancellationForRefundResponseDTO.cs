3530967e740da6b0c33230b49c3ae46a01abf5cb
Karnasinh <karnasinh281@gmail.com>
2024-06-16 19:50:43 -0400

    Adds CancellationForRefundResponseDTO and CancellationForRefundDTO classes to return the response of the cancellation request for refund.

---
+++ HotelBookingAPI/DTOs/CancellationDTOs/CancellationForRefundResponseDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace HotelBookingAPI.DTOs.CancellationDTOs
+{
+    //This class is used to return the response of the cancellation request for refund
+    public class CancellationForRefundResponseDTO
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+        public List<CancellationForRefundDTO> CancellationsToRefund { get; set; }
+    }
+
+    //This class is used to hold the details of the cancellation and refund
+    public class CancellationForRefundDTO
+    {
+        public int CancellationRequestID { get; set; }
+        public int ReservationID { get; set; }
+        public int UserID { get; set; }
+        public string CancellationType { get; set; }
+        public DateTime RequestedOn { get; set; }
+        public string Status { get; set; }
+        public int RefundID { get; set; }
+        public string RefundStatus { get; set; }
+    }
+}
