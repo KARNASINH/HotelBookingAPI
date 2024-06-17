ca9091d872fc3591242088c8d399bf61d5b2eca5
Karnasinh <karnasinh281@gmail.com>
2024-06-16 20:00:09 -0400

    Adds UpdateRefundStatusResponseDTO class to hold the information of the response while updating the refund status.

---
+++ HotelBookingAPI/DTOs/CancellationDTOs/UpdateRefundStatusResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelBookingAPI.DTOs.CancellationDTOs
+{
+    //This class is used to hold the information of the response while updating the refund status.
+    public class UpdateRefundStatusResponseDTO
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+    }
+}
