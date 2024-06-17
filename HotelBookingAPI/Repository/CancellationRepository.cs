116d23cc6c808e7cfa0221c28864be0941518949
Karnasinh <karnasinh281@gmail.com>
2024-06-16 20:03:44 -0400

    Adds SqlConnectionFactory instance to establish the connection with the database.

--- HotelBookingAPI/Repository/CancellationRepository.cs
+++ HotelBookingAPI/Repository/CancellationRepository.cs
@@ -1,7 +1,19 @@
-﻿namespace HotelBookingAPI.Repository
+﻿using HotelBookingAPI.Connection;
+
+namespace HotelBookingAPI.Repository
 {
     //This class is used to handle the database operations related to the cancellation.
     public class CancellationRepository
     {
+        //This is the connection factory class instance.
+        private readonly SqlConnectionFactory _connectionFactory;
+
+        //This is the constructor of the class.
+        public CancellationRepository(SqlConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+
     }
 }
